98790eecddb980720f8dcb135d0457db87ae4d24
Isaac Addis <isaacaddis21@gmail.com>
2018-04-11 16:27:23 +0000

    RIP React, maybe another time

--- Dianying/app.js
+++ Dianying/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var g = require('ger');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
