20f2f06d91d5d7ce82310f4bcd1e3092b4b44fe0
Isaac Addis <isaacaddis21@gmail.com>
2018-04-16 16:23:59 +0000

    Entry form

--- Dianying/public/javascripts/dianying.js
+++ Dianying/public/javascripts/dianying.js
@@ -1,5 +1,10 @@
   $(document).ready(function(){
-    $(".button-collapse").sideNav();
+    // var row = $("div class = 'row'><div class = 'col s6'><input type = 'text' class = 'input-field col s12 white'></div><div class = 'col s6'><i class='fas fa-plus plus'></i></div></div>");
+
+	  $("a").click(function(){
+	    $("#special").append("<div class = 'row'><div class = 'col s6'><input type = 'text' class = 'input-field col s12 white' /></div></div>");
+	  });
+	  $(".button-collapse").sideNav();
     $('select').material_select();
     $('#selection').change(function(){
         var val = $(this).val();
