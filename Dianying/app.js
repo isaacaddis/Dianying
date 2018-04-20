cb4125283493d06357003778416f684193f70c10
Isaac Addis <isaacaddis21@gmail.com>
2018-04-20 16:23:52 +0000

    Backend

--- Dianying/app.js
+++ Dianying/app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var uuid = require('uuid');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var uuid = require('uuid');
 var app = express();
 
 // view engine setup
--- Dianying/routes/users.js
+++ Dianying/routes/users.js
@@ -1,8 +1,34 @@
 var express = require('express');
+var mysql = require('mysql');
 var g = require('ger')
 var esm = new g.MemESM()
 var ger = new g.GER(esm);
-
+var retrieve = "SELECT * FROM entries";
+var con = mysql.createConnection({
+      host: "146.148.51.94",
+      user: "root",
+      password: "",
+      database:"db"
+});
+    
+con.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+});
+var output;
+function retrieveDB(){
+  con.query(retrieve, function (err, result) {
+      if (err) throw err;
+      output = result;
+      console.log("Sucessful retrieval.");
+  });
+}
+function inputDB(input){
+    con.query(input, function (err, result){
+        if(err) throw err;
+        console.log("Sucessful input.")
+    });
+}
 function dianying(namespace){
     return ger.initialize_namespace(String(this));
 }
