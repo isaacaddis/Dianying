5c9de32c65aa2319d26ac81bab8f9e572530f6d5
Isaac Addis <isaacaddis21@gmail.com>
2018-04-23 16:09:38 +0000

    Backend algos

--- Dianying/app.js
+++ Dianying/app.js
@@ -3,11 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var uuid = require('uuid');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var uuid = require('uuid');
 var app = express();
 
 // view engine setup
--- Dianying/routes/users.js
+++ Dianying/routes/users.js
@@ -3,7 +3,8 @@ var mysql = require('mysql');
 var g = require('ger')
 var esm = new g.MemESM()
 var ger = new g.GER(esm);
-var retrieve = "SELECT * FROM entries";
+var shortid = require('shortid');
+var output;
 var con = mysql.createConnection({
       host: "146.148.51.94",
       user: "root",
@@ -15,22 +16,51 @@ con.connect(function(err) {
       if (err) throw err;
       console.log("Connected!");
 });
-var output;
+function convertToJSON(str){
+    return JSON.stringify(str);
+}
+function process(retrieval){
+    var retrieval = this.retrieval;
+    for(var i in retrieval){
+        // Do JSON processing
+        ger.initialize_namespace(retrieval[i][1]);
+        ger.events([{
+          namespace: retrieval[i][1],
+          person: retrieval[i][2],
+          action: retrieval[i][3],
+          thing: retrieval[i][4],
+          expires_at: retrieval[i][5]
+        }]);
+    }
+}
 function retrieveDB(){
+  var retrieve = "SELECT * FROM entries";
   con.query(retrieve, function (err, result) {
       if (err) throw err;
-      output = result;
       console.log("Sucessful retrieval.");
+      return convertToJSON(result);
   });
 }
-function inputDB(input){
-    con.query(input, function (err, result){
+function inputDB(id, namespace, thing){
+    var id = this.id;
+    var namespace = this.namespace;
+    var query = "INSERT INTO entries (namespace, person, action, thing, expires_at) VALUES ?"
+    var values = [namespace,id,"likes", thing, "2020-06-06"]
+    con.query(query, [values],function (err, result){
         if(err) throw err;
-        console.log("Sucessful input.")
+        console.log("Sucessful input.");
     });
 }
-function dianying(namespace){
-    return ger.initialize_namespace(String(this));
+/*
+   Main function
+*/
+function dianying(namespace, thing){
+    var id = shortid.generate();
+    var namespace = this.namespace;
+    var thing = this.thing;
+    inputDB(id, namespace, thing)
+    output = retrieveDB();
+    process(output)
 }
 var router = express.Router();
 
