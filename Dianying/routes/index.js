f7f4c67cbd479e80023e951d79c833a8e3f6c339
Isaac Addis <isaacaddis21@gmail.com>
2018-04-25 16:45:04 +0000

    4/25

--- Dianying/routes/index.js
+++ Dianying/routes/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-// var users = require('./users');
 var router = express.Router();
 var mysql = require('mysql');
 var g = require('ger')
@@ -24,19 +23,41 @@ router.post('/recommend', function(req, res, next) {
         database: "db"
     });
     con.connect(function(err) {
-      if (err) throw err;
-      console.log("Connected!");
+        if (err) throw err;
+        console.log("Connected!");
     });
+
     function dianying(namespace, thing) {
         var id = shortid.generate();
-        console.log("ID: "+ String(id));
         var namespace = namespace;
-        console.log("Namespace: " + namespace);
         var thing = thing;
-        console.log("Thing: "+ thing);
         inputDB(id, namespace, thing);
-        output = retrieveDB();
-        return process(id, namespace, output);
+        var retrieval = retrieveDB();
+        console.log("Starting Processing!");
+        ger.initialize_namespace(namespace)
+            .then(function() {
+                for (var i in retrieval) {
+                    console.log("Iteration: " + i);
+                    if (retrieval[i][1] !== namespace) continue;
+                    ///For testing
+                    console.log("Namespace: " + namespace)
+                    console.log("Person: " + retrieval[i][2])
+                    console.log("Action: " + retrieval[i][3])
+                    console.log("Thing: " + retrieval[i][4])
+
+                    // Train GER
+                    ger.events([{
+                        namespace: namespace,
+                        person: retrieval[i][2],
+                        action: retrieval[i][3],
+                        thing: retrieval[i][4],
+                        expires_at: retrieval[i][5]
+                    }]);
+                }
+                return ger.recommendations_for_person(namespace, id, {actions: {likes: 1}}) || "No possible output, we are still gathering data!"; 
+
+            })
+
     }
 
     function convertToJSON(str) {
@@ -44,12 +65,19 @@ router.post('/recommend', function(req, res, next) {
     }
 
     function process(id, namespace, retrieval) {
+        console.log("Starting Processing");
         var id = id;
         var namespace = namespace;
         ger.initialize_namespace(namespace);
         var retrieval = retrieval;
         for (var i in retrieval) {
+            console.log("Iteration: " + i);
             if (retrieval[i][1] !== namespace) continue;
+            console.log("Namespace: " + namespace)
+            console.log("Person: " + retrieval[i][2])
+            console.log("Action: " + retrieval[i][3])
+            console.log("Thing: " + retrieval[i][4])
+
             // Do JSON processing
             ger.events([{
                 namespace: namespace,
@@ -71,24 +99,29 @@ router.post('/recommend', function(req, res, next) {
         con.query(retrieve, function(err, result) {
             if (err) throw err;
             console.log("Sucessful retrieval.");
+            console.log("Result: " + convertToJSON(result))
             return convertToJSON(result);
         });
     }
 
     function inputDB(id, namespace, thing) {
         var id = id;
+        console.log(id);
         var namespace = namespace;
+        console.log(namespace);
         var thing = thing;
-        var query = "INSERT INTO entries (namespace, person, action, thing, expires_at) VALUES ?";
-        var values = [namespace, id, "likes", thing, "2020-06-06"];
-        con.query(query, [values], function(err, result) {
-            if (err) throw err;
+        console.log(thing);
+        var query = "INSERT INTO entries (namespace, person, action, thing, expires_at) VALUES (" + "'" + namespace + "'" + "," + "'" + id + "'" + "," + "'likes'" + "," + "'" + thing + "'" + "," + "'2020-06-06'" + ")";
+        // var values = [namespace, id, "likes", thing, "2020-06-06"];
+        console.log("Query: " + query);
+        con.query(query, function(err, result) {
+            if (err) console.log(err);
             console.log("Sucessful input.");
         });
     }
     var output = dianying(req.body.namespace, req.body.in);
     res.render('recommend', {
-        output: req.body.namespace
+        output: output
     });
 });
 module.exports = router;
